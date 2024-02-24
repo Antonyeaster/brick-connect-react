81f7a90968e395e82124588edcb269d95855ed67
Antony Easter <antonyeaster@live.co.uk>
2024-02-24 19:10:37 +0000

    Fix typo in posts page

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,7 +11,7 @@ import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
-  const [post, setPosts] = useState({ results: [] });
+  const [post, setPost] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -19,7 +19,7 @@ function PostPage() {
         const [{ data: post }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
         ]);
-        setPosts({ results: [post] });
+        setPost({ results: [post] });
         console.log(post);
       } catch (err) {
         console.log(err);
@@ -32,7 +32,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPosts} postPage />
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
