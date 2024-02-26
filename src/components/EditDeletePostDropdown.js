25977ee5ece1b3cf5ab26178b60db9062b985ac5
Antony Easter <antonyeaster@live.co.uk>
2024-02-26 08:16:07 +0000

    Add edit and delete functionality with pre populated edit form

---
+++ src/components/EditDeletePostDropdown.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/EditDeletePostDropdown.module.css";
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const TreeDots = React.forwardRef(({ onClick }, ref) => (
+  <i
+    className="fas fa-ellipsis"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const EditDeletePostDropdown = ({handleEdit, handleDelete}) => {
+  return (
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle
+        as={TreeDots}
+        id="dropdown-custom-components"
+      ></Dropdown.Toggle>
+
+      <Dropdown.Menu
+        popperConfig={{ strategy: "fixed" }}
+        className="text-center"
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
+        >
+          <i className="fas fa-edit" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash-alt" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
