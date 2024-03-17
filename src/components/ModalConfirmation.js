ce7283cb896464d000203610b22295623272d306
Antony Easter <antonyeaster@live.co.uk>
2024-03-17 12:20:52 +0000

    Add modal confirmation component

---
+++ src/components/ModalConfirmation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const ModalConfirmation = ({ handleMethod, show, setShow, body }) => {
+  return (
+    <Modal show={show} onHide={() => setShow(false)} keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirmation</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="text-center">
+          <strong><p>{body}</p></strong>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShow(false)}>
+          Cancel
+        </Button>
+        <Button onClick={handleMethod} variant="primary">
+          Confirm
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalConfirmation;
