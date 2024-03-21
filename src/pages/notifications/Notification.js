31664e1b8a6cc7f5886271a285b5f8df580d56b0
Antony Easter <antonyeaster@live.co.uk>
2024-03-21 08:32:01 +0000

    Add style to notification card

--- src/pages/notifications/Notification.js
+++ src/pages/notifications/Notification.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { axiosRes } from "../../api/axiosDefaults";
-import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 import toast from "react-hot-toast";
 import ModalConfirmation from "../../components/ModalConfirmation";
 import { Link } from "react-router-dom";
+import btnStyles from "../../styles/Button.module.css"
+import styles from "../../styles/Notifications.module.css"
 
 const Notifications = (props) => {
   const {
@@ -55,56 +57,43 @@ const Notifications = (props) => {
   };
 
   return (
-    <>
+    <div className={styles.Container}>
       <Card>
-        <small className="">{createdAt}</small>
-        <span>
-          <OverlayTrigger
-            placement="top"
-            overlay={<Tooltip>Clear Notification</Tooltip>}
-          >
-            <span
-              aria-label="Clear notification"
-              title="Clear Notification"
-              onClick={handleShowModal}
-            >
-              <i className="fas fa-trash-alt" />
-            </span>
-          </OverlayTrigger>
-          {read ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Mark as unread</Tooltip>}
-            >
-              <span onClick={handleReadStatus}>
-                <i className="fa-solid fa-circle-check" />
-              </span>
-            </OverlayTrigger>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Mark as read</Tooltip>}
-            >
-              <span onClick={handleReadStatus}>
-                <i className="fa-regular fa-circle-check" />
-              </span>
-            </OverlayTrigger>
-          )}
-        </span>
+      <div className="d-flex align-items-center">
+          {!read && <div className={styles.Dot} />}
+          <small className="pl-2 pt-2">{createdAt}</small>
+        </div>
         <Card.Body>
           <Link to={`/profiles/${id}`}>
             <Avatar src={profileImage} height={30} />
           </Link>
           <Card.Text>{text}</Card.Text>
         </Card.Body>
+        <div className={styles.ButtonContainer}>
+            <span>
+            <Button
+            className={`btn-danger mb-2 ${btnStyles.Button}`}
+              aria-label="Delete notification"
+              title="Delete Notification"
+              onClick={handleShowModal}
+            >
+              <i className="fas fa-trash-alt" />
+              </Button>
+            </span>
+          {read ? (
+            <Button className={`mb-2 ${btnStyles.Button} ${btnStyles.BabyBlue}`} onClick={handleReadStatus}>Mark as unread</Button>
+          ) : (
+            <Button className={`mb-2 ${btnStyles.Button} ${btnStyles.Grey}`} onClick={handleReadStatus}>Mark as read</Button>
+          )}
+        </div>
       </Card>
       <ModalConfirmation
         show={showModal}
         setShow={setShowModal}
         handleMethod={handleDelete}
-        body="Clear notification!"
+        body="Delete notification!"
       />
-    </>
+      </div>
   );
 };
 
